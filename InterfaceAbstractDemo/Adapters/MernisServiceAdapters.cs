c4d0beadc8b3519e70f696e29061dfb44b8a2d7c
Erkan özgün <eozgun65@gmail.com>
2024-03-07 21:15:02 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapters.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concretes;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapters : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync( Convert.ToInt64(customer.NationalityId) , customer.FirstName ,customer.LastName , customer.DateOfBirth.Year).Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concretes;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCutomerManager(new MernisServiceAdapters());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(2004, 5, 1), FirstName = "Erkan", LastName = "Özgün", NationalityId = "1" });
+            Console.ReadLine();
+        }
+    }
+}
